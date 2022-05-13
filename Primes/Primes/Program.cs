a12dc5c4693c8a70f43a7a325c61e34653d13092
Mathijs455 <455150@student.fontys.nl>
2022-05-13 16:07:52 +0200

    Create Project

---
+++ Primes/Primes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Timers;
+
+namespace Primes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> primes = new List<int>();
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            for (int i = 0; i < 10000000; i++)
+            {
+                if (isPrime(i))
+                {
+                    primes.Add(i);
+                }
+            }
+
+            timer.Stop();
+            TimeSpan ts = timer.Elapsed;
+            Console.WriteLine($"found {primes.Count} prime numbers it took a total of {ts.TotalSeconds} seconds to calculate");
+
+        }
+
+        static bool isPrime(int n)
+        {
+            // Corner case
+            if (n <= 1)
+                return false;
+
+            // Check from 2 to sqrt(n)
+            for (int i = 2; i < Math.Sqrt(n); i++)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+    }
+}
